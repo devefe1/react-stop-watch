6d10e92911569e784c8c4bb9cc5bcd6a96d0a84c
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-04-28 23:09:16 -0500

    added more code

---
+++ stopwatch/components/count.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+class Count extends Component {
+    constructor(props){
+        super(props);
+//we set the variable time to 0 in the this.state array
+        this.state = {
+            time: 0
+        };
+
+        this.timer = this.timer.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+    }
+//Here i will create variables with events, add if statements and Set intervals
+    timer(event){
+        event.preventDefault();
+        const clickButton = event.target.id;
+        if(clickButton === "start"){
+            let id = setInterval(this.startTimer ,1000);
+            this.setState({clearId:id});
+            // to update state correctly
+        } else if (buttonClicked === 'reset') {
+            this.setState({time:0});
+        } else if (buttonClicked === 'pause') {
+            clearInterval(this.state.clearId);
+        }
+    }
+
+    startTimer(){
+        setInterval(this.setState({ time: this.state.time + 1}), 1000);
+    }
+}
+
+
+
+export default Count;
